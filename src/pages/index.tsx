9bbb3dd3c85ec68af09d8044b7f0dc19c89a550e
kanyanat <kanyanat.kjunior@gmail.com>
2023-03-02 13:56:02 +0700

    test open VDO camera

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-empty-interface */
 import LoginForm from "@/features/login/components/LoginForm";
 import OverlayCalling from "@/features/Overlay-Calling/components/OverlayCalling";
+import VideoChatForm from "@/features/videoChat/components/videoChatForm";
 import { useAppSelector } from "@/stores/store";
 import { useRouter } from "next/router";
 import React, { ReactElement, useEffect, useState } from "react";
@@ -20,7 +21,8 @@ function index({}: Props): ReactElement {
   const [overlayStatus, setOverlayStatus] = useState(false);
   return (
     // <div className="{inter.className}">
-    <div className="{inter.className}">history</div>
+    // <div className="{inter.className}">history</div>
+    <VideoChatForm/>
   );
 }
 
